8c15a5d87a6b9c17ed46a937f8dc90892f94ff6d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-16 19:07:44 +0000

    Deploy avin/react-game15 to github.com/avin/react-game15.git:gh-pages

---
+++ precache-manifest.04787e0c949d16440cd2143342def4eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b193dc83ccfbf3ed273",
+    "url": "./static/css/main.c4efd2d1.chunk.css"
+  },
+  {
+    "revision": "8b193dc83ccfbf3ed273",
+    "url": "./static/js/main.8b193dc8.chunk.js"
+  },
+  {
+    "revision": "cb37ac9125473be02b1f",
+    "url": "./static/css/1.107079f1.chunk.css"
+  },
+  {
+    "revision": "cb37ac9125473be02b1f",
+    "url": "./static/js/1.cb37ac91.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "229e9cbcacf9bf7a65076a794991de17",
+    "url": "./index.html"
+  }
+];
